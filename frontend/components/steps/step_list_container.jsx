4b0e41dbb341aea3d229b4c87490e64731fa4ab0
Josh Turgeon <j_turgeon88@yahoo.com>
2018-07-12 15:24:10 -0500

    fixed import issue

--- frontend/components/steps/step_list_container.jsx
+++ frontend/components/steps/step_list_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import StepList from './step_list';
 import { stepsByTodoId } from '../../reducers/selectors';
+import { receiveStep } from '../../actions/step_actions';
 
 const mapStateToProps = (state, { todo_id }) => ({
   steps: stepsByTodoId(state, todo_id),
