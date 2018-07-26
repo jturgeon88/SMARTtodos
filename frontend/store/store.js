89f4dec7db38ea4ea90ed48731e8a4783e42984c
Josh Turgeon <j_turgeon88@yahoo.com>
2018-07-26 14:39:23 -0500

    Persist state to localStorage

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,6 +3,9 @@ import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
   const store = createStore(rootReducer, preloadedState);
+    store.subscribe(() => {
+      localStorage.state = JSON.stringify(store.getState());
+    })
   return store;
 };
 
